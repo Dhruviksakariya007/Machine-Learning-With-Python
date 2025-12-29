3b7280c9bb53de7611b5f31b431e27ce11e9e267
Dhruvik <dhruviksakariya007@email.com>
2025-12-29 15:44:04 +0100

    file handling in calc

--- calc.py
+++ calc.py
@@ -1,6 +1,9 @@
+def createFile():
+    open("calc_history.txt", 'x').close()
+
 def calc(n1, n2, op):
     if op == '*':
-        return print(f"Multiplication: {n1 * n2}")
+        return print(f"{n1} * {n2}: {n1 * n2}")
     elif op == '+':
         return print(f"Addition: {n1 + n2}")
     elif op == '-':
@@ -11,15 +14,16 @@ def calc(n1, n2, op):
         return print("Invalid Operation")
 
 def main():
-    n1 = input("Enter first number: ")
-    n2 = input("Enter second number: ")
-    op = input("Enter Operator (*, +, -, /): ")
-
-    result = calc(float(n1),float(n2),op)
-    if int(result) == result:
-        print(f"Result: {int(result)}")
+    user_input = input("What do you want to do? (Calc, history, clear history): ")
+    if user_input.lower() != "calc":
+        print("This feature is not implemented yet.")
+        return
     else:
-        print(f"Result: {result}")
+        n1 = input("Enter first number: ")
+        n2 = input("Enter second number: ")
+        op = input("Enter Operator (*, +, -, /): ")
+        
+        calc(float(n1),float(n2),op)
 
 if __name__ == "__main__":
     main()
