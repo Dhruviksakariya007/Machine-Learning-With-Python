ea09a1d90dbe9915b5ecde98f3b48d364560b098
Dhruvik <dhruviksakariya007@email.com>
2025-12-29 16:03:02 +0100

    complete calculator

--- calc.py
+++ calc.py
@@ -1,29 +1,62 @@
+FILE = "calc_history.txt"
+
 def createFile():
-    open("calc_history.txt", 'x').close()
+    open(FILE, 'x').close()
+
+def history():
+    his = open(FILE, 'r')
+    print(his.read())
+    his.close()
+
+def saveHistory(result):
+    his = open(FILE, 'a')
+    his.write(f"{result}\n")
+    his.close()
+
+def clearHistory():
+    open(FILE, 'w').close()
 
 def calc(n1, n2, op):
     if op == '*':
-        return print(f"{n1} * {n2}: {n1 * n2}")
+        return f"{n1} * {n2}: {n1 * n2}"
     elif op == '+':
-        return print(f"Addition: {n1 + n2}")
+        return f"{n1} + {n2}: : {n1 + n2}"
     elif op == '-':
-        return print(f"Subtraction: {n1 - n2}")
+        return f"{n1} - {n2}: {n1 - n2}"
     elif op == '/': 
-        return print(f"Division: {n1 / n2}")
+        return f"{n1} / {n2}: {n1 / n2}"
     else:
         return print("Invalid Operation")
 
 def main():
-    user_input = input("What do you want to do? (Calc, history, clear history): ")
-    if user_input.lower() != "calc":
-        print("This feature is not implemented yet.")
-        return
-    else:
-        n1 = input("Enter first number: ")
-        n2 = input("Enter second number: ")
-        op = input("Enter Operator (*, +, -, /): ")
+
+    while True:
+        user_input = input("What do you want to do? (Calc, history, clear history, exit): ")
+
+        if user_input.lower() == "history":
+            history()
         
-        calc(float(n1),float(n2),op)
+        elif user_input.lower() == "clear history" or user_input.lower() == "clearhistory" or user_input.lower() == "clear":
+            clearHistory()
+            print("History cleared.")
+            
+        elif user_input.lower() == "calc":
+            n1 = input("Enter first number: ")
+            n2 = input("Enter second number: ")
+            op = input("Enter Operator (*, +, -, /): ")
+            
+            result = calc(float(n1),float(n2),op)
+            print(result)
+            saveHistory(result)
+        
+        elif user_input.lower() == "exit":
+            print("Exiting...")
+            break
+
+        else:
+            print("INVALID INPUT \nExiting...")
+            break
 
 if __name__ == "__main__":
     main()
+    
